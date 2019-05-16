07d936c743dce8651734f7c640231a345d314af6
programmer-zhang <chinajnzhang@hotmail.com>
2019-05-16 23:49:58 +0800

    updata colud

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
     // 登录
     wx.login({
@@ -32,6 +32,13 @@ App({
         }
       }
     })
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }    
   },
   globalData: {
     userInfo: null
