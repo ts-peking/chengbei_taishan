6bbb6ee2a53979b60d9e2ef1a61202c00ecf4214
programmer-zhang <chinajnzhang@hotmail.com>
2019-05-17 00:12:51 +0800

    updata cloud

---
+++ miniprogram/pages/logs/logs.js
@@ -0,0 +1,31 @@
+//logs.js
+const util = require('../../utils/util.js')
+const app = getApp()
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  },
+
+  cloudGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+  },  
+})
