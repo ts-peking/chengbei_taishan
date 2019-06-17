93f6ba446761fdce87adc3d1b9331e7b3a6af14d
programmer-zhang <chinajnzhang@hotmail.com>
2019-06-17 23:36:11 +0800

    updata test getdatabase

--- cloudfunctions/gettestdatabase/index.js
+++ cloudfunctions/gettestdatabase/index.js
@@ -7,7 +7,7 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
-const db = wx.cloud.database() // 初始化数据库
+const db = cloud.database() // 初始化数据库
 const test = db.collection('testdatabase')
 
 console.log('testdatabase',test)
@@ -17,10 +17,9 @@ exports.main = (event, context) => {
   // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）
   const wxContext = cloud.getWXContext()
 
-  // return {
-  //   event,
-  //   openid: wxContext.OPENID,
-  //   appid: wxContext.APPID,
-  //   unionid: wxContext.UNIONID,
-  // }
+  return {
+    event,
+    wxContext,
+    test
+  }
 }
