0433a5ba0467815a4fdd73174918b15a4460afde
programmer-zhang <chinajnzhang@hotmail.com>
2019-07-17 23:59:04 +0800

    updata activit

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -19,51 +19,15 @@ Page({
     },
     selectTab: 'play',
     watchGameList: [
-        {
-            activityImg: '',
-            activityName: '鲁能VS国安',
-            activityLocation: '北京工人体育场',
-            activityPoi: '',//留作接入腾讯地图
-            activityDate: 1543759535822,
-            activityPrice: '100元',
-            activityStatus: 'apply',//报名状态：apply.可报名，registered，已报名，finished.已结束，underway.进行中
-        },
-        {
-            activityImg: '',
-            activityName: '鲁能VS权健',
-            activityLocation: '海教园体育场',
-            activityPoi: '',//留作接入腾讯地图
-            activityDate: 1543759535822,
-            activityPrice: 'AA制',
-            activityStatus: 'apply',//报名状态：apply.可报名，registered，已报名，finished.已结束，underway.进行中
-        },
-        {
-            activityImg: '',
-            activityName: '鲁能VS泰达',
-            activityLocation: '水滴',
-            activityPoi: '',//留作接入腾讯地图
-            activityDate: 1548759535822,
-            activityPrice: '免费',
-            activityStatus: 'apply',//报名状态：apply.可报名，registered，已报名，finished.已结束，underway.进行中
-        },
-        {
-            activityImg: '',
-            activityName: '鲁能VS国安',
-            activityLocation: '北京工人体育场',
-            activityPoi: '',//留作接入腾讯地图
-            activityDate: 1543959535822,
-            activityPrice: '100元',
-            activityStatus: 'registered',//报名状态：apply.可报名，registered，已报名，finished.已结束，underway.进行中
-        },
-        {
-            activityImg: '',
-            activityName: '鲁能VS河北',
-            activityLocation: '廊坊',
-            activityPoi: '',//留作接入腾讯地图
-            activityDate: 1543759535822,
-            activityPrice: 'AA制',
-            activityStatus: 'finished',//报名状态：apply.可报名，registered，已报名，finished.已结束，underway.进行中
-        },
+        // {
+        //     activityImg: '',
+        //     activityName: '鲁能VS国安',
+        //     activityLocation: '北京工人体育场',
+        //     activityPoi: '',//留作接入腾讯地图
+        //     activityDate: 1543759535822,
+        //     activityPrice: '100元',
+        //     activityStatus: 'apply',//报名状态：apply.可报名，registered，已报名，finished.已结束，underway.进行中
+        // }
     ],
     activityList: [],
     pageSize: 10,
