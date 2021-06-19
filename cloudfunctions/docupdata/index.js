0637241f0c0f23e232ed64bf6f384d4c8ae18d31
programmer-zhang <chinajnzhang@hotmail.com>
2021-06-19 17:06:19 +0800

    eslint formate code

--- cloudfunctions/docupdata/index.js
+++ cloudfunctions/docupdata/index.js
@@ -1,38 +1,37 @@
-// 云函数模板
-// 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
-// 创建更新数据库云函数
-const cloud = require('wx-server-sdk')
+/**
+ * @file 更新云数据库 云函数
+ * @author savuer
+ */
+const cloud = require('wx-server-sdk');
 
 // 初始化 cloud
-cloud.init()
+cloud.init();
 // 全局云数据库环境ID  测试环境 'taishanchengbei' 生产环境 'prodenv-2sbjk'
-const db = cloud.database({ env: 'prodenv-2sbjk' })
+const db = cloud.database({
+    env: 'prodenv-2sbjk'
+});
 
 /**
- * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
- * 
- * event 参数包含小程序端调用传入的 data
- 参数：
-  event = {
-    dbId: dbId, // 数据库ID
-    docId: docId, // 数据库数据ID
-    data: data // 存储的数据库集合
-  }
- * 
+ * 更新 指定dbId 指定docId 中的数据
+ *
+ * @param {Object} event 参数包含小程序端调用传入的 data event = {dbId: dbId, // 数据库ID docId: docId, // 数据库数据ID data: data // 存储的数据库集合}
+ * @param {Object} context context
+ * @return {Function}
  */
 exports.main = async (event, context) => {
-  try {
-    return await db.collection(event.dbId).doc(event.docId).update({
-     // data 传入需要局部更新的数据
-      data: event.data,
-      success: function(res) {
-        console.log('docupdata成功', res)
-      },
-      fail: function(err) {
-        console.log('docupdata失败', err)
-      }
-    })
-  } catch (e) {
-    console.error(e)
-  }
-}
+    try {
+        return await db.collection(event.dbId).doc(event.docId).update({
+                // data 传入需要局部更新的数据
+                data: event.data,
+                success(res) {
+                    console.log('docupdata成功', res);
+                },
+                fail(err) {
+                    console.log('docupdata失败', err);
+                }
+            });
+    }
+    catch (e) {
+        console.error(e);
+    }
+};
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,61 +1,68 @@
-//app.js
+/**
+ * @file app.js
+ * @author savuer
+ */
+
+/* global wx App Page */
+
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
+    onLaunch() {
+        // 展示本地存储能力
+        // var logs = wx.getStorageSync('logs') || []
+        // logs.unshift(Date.now())
+        // wx.setStorageSync('logs', logs)
 
-    // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
-    // // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
+        // 登录
+        // wx.login({
+        //   success: res => {
+        //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        //   }
+        // })
+        // // 获取用户信息
+        // wx.getSetting({
+        //   success: res => {
+        //     if (res.authSetting['scope.userInfo']) {
+        //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        //       wx.getUserInfo({
+        //         success: res => {
+        //           // 可以将 res 发送给后台解码出 unionId
+        //           this.globalData.userInfo = res.userInfo
 
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        env: 'prodenv-2sbjk',
-        traceUser: true,
-      })
-      // 获取openId
-      wx.cloud.callFunction({
-        name: 'login',
-        data: {},
-        success: res => {
-          console.log('[云函数] [login] user openid: ', res.result.openid)
-          this.globalData.openid = res.result.openid
-        },
-        fail: err => {
-          console.error('[云函数] [login] 调用失败', err)
+        //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        //           // 所以此处加入 callback 以防止这种情况
+        //           if (this.userInfoReadyCallback) {
+        //             this.userInfoReadyCallback(res)
+        //           }
+        //         }
+        //       })
+        //     }
+        //   }
+        // })
+        if (!wx.cloud) {
+            console.error('请使用 2.2.3 或以上的基础库以使用云能力');
         }
-      })      
+        else {
+            wx.cloud.init({
+                env: 'prodenv-2sbjk',
+                traceUser: true
+            });
+            // 获取openId
+            wx.cloud.callFunction({
+                name: 'login',
+                data: {},
+                success: res => {
+                    console.log('[云函数] [login] user openid: ', res.result.openid);
+                    this.globalData.openid = res.result.openid;
+                },
+                fail: err => {
+                    console.error('[云函数] [login] 调用失败', err);
+                }
+            });
+        }
+    },
+    globalData: {
+        userInfo: null,
+        openid: null,
+        databaseEnv: 'prodenv-2sbjk'     // 全局云数据库环境ID  测试环境 'taishanchengbei' 生产环境 'prodenv-2sbjk'
     }
-  },
-  globalData: {
-    userInfo: null,
-    openid: null,
-    databaseEnv: 'prodenv-2sbjk', // 全局云数据库环境ID  测试环境 'taishanchengbei' 生产环境 'prodenv-2sbjk'
-  }
-})
+});
