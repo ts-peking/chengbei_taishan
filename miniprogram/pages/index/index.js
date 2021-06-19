cdede529d6d3ade05ca4702e2d08373c0a52fa35
programmer-zhang <chinajnzhang@hotmail.com>
2021-06-19 16:38:52 +0800

    eslint formate code

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,66 +1,76 @@
-//index.js
-//获取应用实例
-const app = getApp()
+/**
+ * @file 首页
+ * @author savuer
+ */
+
+/* global wx getApp Page */
+
+const app = getApp();
 
 Page({
-  data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    wx.setNavigationBarTitle({
-      title: "永不倒的泰山"
-    })
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    data: {
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo')
+    },
+    // 事件处理函数
+    bindViewTap() {
+        wx.navigateTo({
+            url: '../logs/logs'
+        });
+    },
+    onLoad() {
+        wx.setNavigationBarTitle({
+            title: '永不倒的泰山'
+        });
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            });
         }
-      })
+        else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                });
+            };
+        }
+        else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo;
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    });
+                }
+            });
+        }
+    },
+    getUserInfo(e) {
+        if (e.detail && !e.detail.userInfo) {
+            return;
+        }
+
+        app.globalData.userInfo = e.detail.userInfo;
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        });
+    },
+    goLogin() {
+        wx.navigateTo({
+            url: '/pages/user/user'
+        });
+    },
+    goActivityList() {
+        wx.navigateTo({
+            url: '/pages/activity/activity'
+        });
     }
-  },
-  getUserInfo: function(e) {
-    if (e.detail && !e.detail.userInfo) { return }
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  goLogin: function(e) {
-    wx.navigateTo({
-      url:'/pages/user/user'
-    })
-  },
-  goActivityList: function() {
-    wx.navigateTo({
-      url: '/pages/activity/activity'
-    })
-  }
-})
+});
--- miniprogram/pages/logs/logs.js
+++ miniprogram/pages/logs/logs.js
@@ -1,61 +1,69 @@
-//logs.js
-const util = require('../../utils/util.js')
-const app = getApp()
-const db = wx.cloud.database({ env: app.globalData.databaseEnv }) // 初始化数据库
+/**
+ * @file LOG测试页
+ * @author savuer
+ */
+/* global wx getApp Page */
+
+// logs.js
+const util = require('../../utils/util.js');
+const app = getApp();
+const db = wx.cloud.database({
+    env: app.globalData.databaseEnv
+}); // 初始化数据库
 
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  },
+    data: {
+        logs: []
+    },
+    onLoad() {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map(log => {
+                return util.formatTime(new Date(log));
+            })
+        });
+    },
 
-  getTestDataBase: function() {
-    db.collection('testdatabase').orderBy('vipid', 'desc').get({
-      success: function(res) {
-        console.log('getTestDataBase', res.data)
-      },
-      fail: function(err) {
-        console.log('error', err)
-      }
-    })
-  },
+    getTestDataBase() {
+        db.collection('testdatabase').orderBy('vipid', 'desc').get({
+            success(res) {
+                console.log('getTestDataBase', res.data);
+            },
+            fail(err) {
+                console.log('error', err);
+            }
+        });
+    },
 
-  addTestDataBase: function() {
-    let params = {
-      _personalcardid: '123',
-      _realname: '测试3',
-      _selectteamid: 1,
-      _vipid: 1319
+    addTestDataBase() {
+        let params = {
+            '_personalcardid': '123',
+            '_realname': '测试3',
+            '_selectteamid': 1,
+            '_vipid': 1319
+        };
+        db.collection('testdatabase').add({
+            data: params,
+            success(res) {
+                console.log('getTestDataBase', res);
+            },
+            fail(err) {
+                console.log('error', err);
+            }
+        });
+    },
+
+    cloudGetOpenid() {
+        // 调用云函数
+        wx.cloud.callFunction({
+            name: 'login',
+            data: {},
+            success: res => {
+                console.log('[云函数] [login] user openid: ', res.result.openid);
+                app.globalData.openid = res.result.openid;
+            },
+            fail: err => {
+                console.error('[云函数] [login] 调用失败', err);
+            }
+        });
     }
-    db.collection('testdatabase').add({
-      data: params,
-      success: function(res) {
-        console.log('getTestDataBase', res)
-      },
-      fail: function(err) {
-        console.log('error', err)
-      }
-    })    
-  },
-
-  cloudGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-      }
-    })
-  },  
-})
+});
